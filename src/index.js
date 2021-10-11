f820903179036a1cd697a65ae3ac373bde2df90b
Kemal <demirelkd0696@gmail.com>
2021-10-11 17:38:25 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './home-work-1/index.css';
+import App from './home-work-1/App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
