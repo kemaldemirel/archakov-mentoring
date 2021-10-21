b96bca91b82d7f3ea65cb9a49f32e5040e8e25a7
Kemal <demirelkd0696@gmail.com>
2021-10-21 15:50:42 +0300

    Сделал дз 6

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from './home-work-6/App.jsx';
 
-ReactDOM.render( <
-  App / > ,
+ReactDOM.render( 
+  <Router>
+    <App / >
+  </Router> ,
   document.getElementById('root')
 );
